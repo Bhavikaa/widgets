2ebefb1156b1cb2db2f13f7ab486bdd460ee56e9
Bhavika Joshi <pandya.bhavika@gmail.com>
2020-10-19 14:33:25 +0530

    Created acoordion widget

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+    {
+        title:"What is React",
+        content:"React is a front end javascript framework"
+    },
+    {
+        title:"Why to use react?",
+        content:"It is favourite among engineers"
+    },
+    {
+        title:"How to use React?",
+        content:"You use React by creating component"
+    }
+];
 
-export default App;
+export default ()=>{
+    return(
+        <div><Accordion items={items}/></div>
+    )
+};
